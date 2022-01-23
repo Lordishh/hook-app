5a0dd7f89ea36ef2be7cf2e99ba36b5617874be6
Enuel <enuel.monzon@gmail.com>
2022-01-23 00:19:38 -0300

    Lista de quehaceres

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-// import React from "react";
-// import ReactDOM from "react-dom";
+import React from "react";
+import ReactDOM from "react-dom";
 
 // // import HookApp from "./HookApp";
 // // import CounterApp from "./components/01-useState/CounterApp";
@@ -14,7 +14,7 @@
 // // import MemoHook from "./components/06-memos/MemoHook";
 // // import CallbackHook from "./components/06-memos/CallbackHook";
 // import { Padre } from "./components/07-tarea-memo/Padre";
+import { TodoApp } from "./components/08-useReducer/TodoApp";
 
-// ReactDOM.render(<Padre />, document.getElementById("root"));
+ReactDOM.render(<TodoApp />, document.getElementById("root"));
 
-import "./components/08-useReducer/intro-reducer";
