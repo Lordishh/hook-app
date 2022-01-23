a6557544e1ad88d704c9cb5aa0906bd92734db33
Enuel <enuel.monzon@gmail.com>
2022-01-23 20:05:00 -0300

    Agregar nuevo TODO

--- src/components/08-useReducer/TodoApp.js
+++ src/components/08-useReducer/TodoApp.js
@@ -4,59 +4,77 @@ import { todoReducer } from "./todoReducer";
 import "./styles.css";
 
 const initialState = [
-    {
-        id: new Date().getTime(),
-        desc: "Aprender React",
-        done: false,
-    },
+  {
+    id: new Date().getTime(),
+    desc: "Aprender React",
+    done: false,
+  },
 ];
 
 export const TodoApp = () => {
-    const [todos] = useReducer(todoReducer, initialState);
+  const [todos, dispatch] = useReducer(todoReducer, initialState);
 
-    console.log(todos);
-    return (
-        <div>
-            <h1>TodoApp ( {todos.length} )</h1>
-            <hr />
+  console.log(todos);
 
-            <div className="row">
-                <div className="col-7">
-                    <ul className="list-group list-group-flush">
-                        {todos.map((todo, i) => (
-                            <li key={todo.id} className="list-group-item">
-                                <p className="text-center">
-                                    {i + 1}. {todo.desc}
-                                </p>
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-                                <button className="btn btn-danger">
-                                    {" "}
-                                    Borrar
-                                </button>
-                            </li>
-                        ))}
-                    </ul>
-                </div>
+    const newTodo = {
+      id: new Date().getTime(),
+      desc: "Nueva tarea",
+      done: false,
+    };
 
-                <div className="col-5">
-                    <h4>Agregar TODO</h4>
-                    <hr />
+    const action = {
+      type: "add",
+      payload: newTodo,
+    };
 
-                    <form>
-                        <input
-                            type="text"
-                            name="description"
-                            className="form-control"
-                            placeholder="Aprender..."
-                            autoComplete="off"
-                        />
+    dispatch(action);
+  };
 
-                        <button className="btn btn-outline-primary mt-1 btn-block">
-                            Agregar
-                        </button>
-                    </form>
-                </div>
-            </div>
+  return (
+    <div>
+      <h1>TodoApp ( {todos.length} )</h1>
+      <hr />
+
+      <div className="row">
+        <div className="col-7">
+          <ul className="list-group list-group-flush">
+            {todos.map((todo, i) => (
+              <li key={todo.id} className="list-group-item">
+                <p className="text-center">
+                  {i + 1}. {todo.desc}
+                </p>
+
+                <button className="btn btn-danger"> Borrar</button>
+              </li>
+            ))}
+          </ul>
         </div>
-    );
+
+        <div className="col-5">
+          <h4>Agregar TODO</h4>
+          <hr />
+
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              name="description"
+              className="form-control"
+              placeholder="Aprender..."
+              autoComplete="off"
+            />
+
+            <button
+              type="submit"
+              className="btn btn-outline-primary mt-1 btn-block"
+            >
+              Agregar
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
--- src/components/08-useReducer/todoReducer.js
+++ src/components/08-useReducer/todoReducer.js
@@ -1,9 +1,9 @@
 export const todoReducer = (state = [], action) => {
-    switch (action.type) {
-        case "add":
-            break;
+  switch (action.type) {
+    case "add":
+      return [...state, action.payload];
 
-        default:
-            return state;
-    }
+    default:
+      return state;
+  }
 };
